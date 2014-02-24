333fc02b6781a51ab6a688243217b120d3aa781e
Caleb <pwwang.hku@gmail.com>
2014-02-24 22:07:35 +0800

    Finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:index, :edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
+  
   def new
     @user = User.new
   end
@@ -17,11 +21,51 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+  
+  def edit
+  end
+
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_url
+  end
 
   private
 
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                      :password_confirmation)
+    end
+
+    # Before filters
+
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
+    
+    def admin_user
+      redirect_to(root_path) unless current_user.admin?
     end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -17,8 +17,10 @@ describe User do
   it { should respond_to(:password_confirmation) }
   it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
+  it { should respond_to(:admin) }
   
   it { should be_valid }
+  it { should_not be_admin }
   
   describe "when email is not present" do
     before { @user.email = " " }
@@ -99,4 +101,14 @@ describe User do
     before { @user.save }
     its(:remember_token) { should_not be_blank }
   end
+  
+  describe "with admin attribute set to 'true'" do
+    before do
+      @user.save!
+      @user.toggle!(:admin)
+    end
+
+    it { should be_admin }
+  end
+    
 end
