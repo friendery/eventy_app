4e21bb3a7a09992917d6715e7a2f535d6757cea5
Caleb <pwwang.hku@gmail.com>
2014-03-13 20:31:59 +0800

    Event page and event delete funciton

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,6 @@
 class EventsController < ApplicationController
   before_action :signed_in_user
+  before_action :correct_user,  only: :destroy
 
   def new
     @event = Event.new
@@ -14,13 +15,25 @@ class EventsController < ApplicationController
       render 'static_pages/home'
     end
   end
+  
+  def show
+    @event = current_user.events.find_by(id: params[:id])
+  end
 
   def destroy
+    @user = current_user
+    @event.destroy
+    redirect_to @user
   end
 
   private
 
     def event_params
       params.require(:event).permit(:title, :description)
+    end
+    
+    def correct_user
+      @event = current_user.events.find_by(id: params[:id])
+      redirect_to root_url if @event.nil?
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 SampleApp::Application.routes.draw do
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
-  resources :events,   only: [:new, :create, :destroy]
+  resources :events
   root to: 'static_pages#home'
   match '/signup',  to: 'users#new',            via: 'get'
   match '/signin',  to: 'sessions#new',         via: 'get'
