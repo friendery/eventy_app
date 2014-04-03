d63424f4ed946526cdcb4fbd7f90b2fda9f58a36
Isaac Chan <isaactinghong@gmail.com>
2014-04-03 18:14:33 +0800

    Layout: changed to street street (instead of lat lng) & Layout change to event show page

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
@@ -12,82 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140403090042) do
-
-  create_table "comments", force: true do |t|
-    t.integer  "user_id"
-    t.integer  "event_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "title"
-    t.text     "comment"
-  end
-
-  create_table "eventjoinings", force: true do |t|
-    t.integer  "user_id"
-    t.integer  "event_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "status",     default: "pending"
-  end
-
-  add_index "eventjoinings", ["event_id"], name: "index_eventjoinings_on_event_id"
-  add_index "eventjoinings", ["user_id", "event_id"], name: "index_eventjoinings_on_user_id_and_event_id", unique: true
-  add_index "eventjoinings", ["user_id"], name: "index_eventjoinings_on_user_id"
-
-  create_table "events", force: true do |t|
-    t.string   "title"
-    t.string   "description"
-    t.integer  "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "avatar"
-    t.string   "date"
-    t.integer  "capacity"
-    t.text     "region"
-    t.text     "street"
-  end
-
-  add_index "events", ["user_id", "created_at"], name: "index_events_on_user_id_and_created_at"
-
-  create_table "friendships", force: true do |t|
-    t.integer  "user_id"
-    t.integer  "friend_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "status",     default: "request"
-  end
-
-  create_table "users", force: true do |t|
-    t.string   "name"
-    t.string   "email"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "password_digest"
-    t.string   "remember_token"
-    t.boolean  "admin",           default: false
-    t.string   "photo"
-  end
-
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["remember_token"], name: "index_users_on_remember_token"
-
-end
-=======
-# encoding: UTF-8
-# This file is auto-generated from the current state of the database. Instead
-# of editing this file, please use the migrations feature of Active Record to
-# incrementally modify your database, and then regenerate this schema definition.
-#
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
-#
-# It's strongly recommended that you check this file into your version control system.
-
-ActiveRecord::Schema.define(version: 20140330091821) do
+ActiveRecord::Schema.define(version: 20140403091358) do
 
   create_table "comments", force: true do |t|
     t.integer  "user_id"
@@ -121,10 +45,9 @@ ActiveRecord::Schema.define(version: 20140330091821) do
     t.string   "avatar"
     t.string   "date"
     t.integer  "capacity",                    default: 0
-    t.text     "lat"
-    t.text     "lng"
     t.text     "administrative_area_level_1"
     t.text     "region"
+    t.text     "street"
   end
 
   add_index "events", ["user_id", "created_at"], name: "index_events_on_user_id_and_created_at"
@@ -152,4 +75,3 @@ ActiveRecord::Schema.define(version: 20140330091821) do
   add_index "users", ["remember_token"], name: "index_users_on_remember_token"
 
 end
->>>>>>> 1df02afd5f84138e420707249df52af973d33bb3
