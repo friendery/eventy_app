04b4394b2d8061acebdf4d7aeb6d66bdea0fe550
Caleb <pwwang.hku@gmail.com>
2014-04-03 17:04:41 +0800

    add street address

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140330091821) do
+ActiveRecord::Schema.define(version: 20140403090042) do
 
   create_table "comments", force: true do |t|
     t.integer  "user_id"
@@ -43,9 +43,8 @@ ActiveRecord::Schema.define(version: 20140330091821) do
     t.string   "avatar"
     t.string   "date"
     t.integer  "capacity"
-    t.text     "lat"
-    t.text     "lng"
     t.text     "region"
+    t.text     "street"
   end
 
   add_index "events", ["user_id", "created_at"], name: "index_events_on_user_id_and_created_at"
