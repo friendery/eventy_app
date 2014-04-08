058740aeb183e6c4385072affdc1134e4d82fe58
Isaac Chan <isaactinghong@gmail.com>
2014-04-08 17:37:38 +0800

    Layout: added modal for WHEN search, fix sprite path

--- Gemfile
+++ Gemfile
@@ -33,6 +33,7 @@ end
 gem 'uglifier', '2.1.1'
 gem 'coffee-rails', '4.0.1'
 gem 'jquery-rails', '2.2.1'
+gem 'jquery-ui-rails'
 gem 'turbolinks', '1.1.1'
 gem 'jbuilder', '1.0.2'
 
--- db/schema.rb
+++ db/schema.rb
@@ -40,9 +40,12 @@ ActiveRecord::Schema.define(version: 20140408061856) do
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image_uid"
+    t.string   "image_name"
     t.string   "avatar"
     t.string   "date"
-    t.integer  "capacity"
+    t.integer  "capacity",                    default: 0
+    t.text     "administrative_area_level_1"
     t.text     "region"
     t.text     "street"
   end
