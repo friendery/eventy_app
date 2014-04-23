8a681040a065be70141335951358fbc578ebd6b7
Caleb <pwwang.hku@gmail.com>
2014-04-23 21:55:47 +0800

    Change all Friendery to Eventy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       sign_in @user
-      flash[:success] = "Welcome to Friendery!"
+      flash[:success] = "Welcome to Eventy!"
       redirect_to @user
     else
       render 'new'
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,5 +1,5 @@
 def full_title(page_title)
-  base_title = "Friendery Sample App"
+  base_title = "Eventy"
   if page_title.empty?
     base_title
   else
