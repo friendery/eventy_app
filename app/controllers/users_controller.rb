f7824fc85096d123cc1677cded502f9f3ab12e19
Caleb <pwwang.hku@gmail.com>
2014-05-01 17:31:36 +0800

    PG find bug fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -51,11 +51,11 @@ class UsersController < ApplicationController
   end
   
   def editdetails
-    @user = User.find_by(params[:id])
+    @user = User.find(params[:id])
   end
   
   def updatedetails
-    @user = User.find_by(params[:id])
+    @user = User.find(params[:id])
     if @user.update_attributes(detail_params)
       flash[:success] = "Profile updated"
       redirect_to @user
