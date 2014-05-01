6d39a83b2b1967ba80f12225d14b875169720d18
Caleb <pwwang.hku@gmail.com>
2014-05-01 13:52:45 +0800

    increase title length

--- app/models/event.rb
+++ app/models/event.rb
@@ -7,7 +7,7 @@ class Event < ActiveRecord::Base
   has_many :rates, dependent: :destroy
   
   validates :description, presence: true
-  validates :title,       presence: true, length: { maximum: 2 }
+  validates :title,       presence: true, length: { maximum: 50 }
   validates :user_id,     presence: true
   validates :capacity,    presence: true, numericality: { only_integer: true, greater_than: 0 }
   validates :date,        presence: true
